3a4f41b83b654bcf8553d44889c8a410ffd0bc84
makogra <pietrzak.maciek.pv@gmail.com>
2022-06-20 17:15:04 +0200

    added TileSet

---
+++ app/src/main/java/com/mako/patterngeneratorwfc/TileSet.java
@@ -0,0 +1,14 @@
+package com.mako.patterngeneratorwfc;
+
+import java.util.List;
+
+public class TileSet {
+
+    private int[][] valueGrid;
+    private List<Character> valueToChar;
+
+    TileSet(char[][] inputGrid){
+        //TODO make valuGrid and valueToCharMap
+    }
+
+}
