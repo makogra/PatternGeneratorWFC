b0e2041fcfb1ad2b6c202ee4095fa3efc1167c44
makogra <pietrzak.maciek.pv@gmail.com>
2022-06-23 14:51:20 +0200

    added sample buttons

--- app/src/main/java/com/mako/patterngeneratorwfc/datamodels/SettingsTileSetViewModel.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/datamodels/SettingsTileSetViewModel.java
@@ -4,5 +4,21 @@ import androidx.lifecycle.ViewModel;
 
 public class SettingsTileSetViewModel extends ViewModel {
 
+    private int sample = 0;
 
+    public int getSample() {
+        return sample;
+    }
+
+    public void setSample(int sample) {
+        this.sample = sample;
+    }
+
+    public void increaseSample(){
+        this.sample++;
+    }
+
+    public void decreaseSample(){
+        this.sample--;
+    }
 }
--- app/src/main/java/com/mako/patterngeneratorwfc/ui/SettingsTileSetFragment.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/ui/SettingsTileSetFragment.java
@@ -8,6 +8,7 @@ import androidx.lifecycle.ViewModelProvider;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.mako.patterngeneratorwfc.R;
@@ -43,10 +44,24 @@ public class SettingsTileSetFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings_tile_set, container, false);
-
+        Button sampleIncrease = view.findViewById(R.id.settings_tile_set_test_increase_btn);
+        sampleIncrease.setOnClickListener(v -> {
+            settingsTileSetViewModel.increaseSample();
+            updateSample();
+        });
+        Button sampleDecrease = view.findViewById(R.id.settings_tile_set_test_decrease_btn);
+        sampleIncrease.setOnClickListener(v -> {
+            settingsTileSetViewModel.decreaseSample();
+            updateSample();
+        });
         //TextView textView = view.findViewById(R.id.fragment_settings_tile_set_test_text_view);
         //textView.setText("" + tileSetViewModel.getCurrentId());
         return view;
+    }
+
+    private void updateSample(){
+        TextView sample = requireView().findViewById(R.id.test_number);
+        sample.setText(Integer.toString(settingsTileSetViewModel.getSample()));
     }
 
     @Override
@@ -54,5 +69,6 @@ public class SettingsTileSetFragment extends Fragment {
         super.onResume();
         TextView textView = this.requireView().findViewById(R.id.fragment_settings_tile_set_test_text_view);
         textView.setText("" + tileSetViewModel.getCurrentId());
+        updateSample();
     }
 }
