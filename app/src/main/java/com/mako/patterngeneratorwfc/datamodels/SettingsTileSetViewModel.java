16cc6feca730deff21c4390d95d322c99fb59f79
makogra <pietrzak.maciek.pv@gmail.com>
2022-06-23 15:29:36 +0200

    added sample limits - min,max

--- app/src/main/java/com/mako/patterngeneratorwfc/datamodels/SettingsTileSetViewModel.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/datamodels/SettingsTileSetViewModel.java
@@ -5,20 +5,24 @@ import androidx.lifecycle.ViewModel;
 public class SettingsTileSetViewModel extends ViewModel {
 
     private int sample = 0;
+    private int sampleMin = 0;
+    private int sampleMax = 10;
 
     public int getSample() {
         return sample;
     }
 
     public void setSample(int sample) {
+        if (sample < sampleMin || sample > sampleMax)
+            return;
         this.sample = sample;
     }
 
     public void increaseSample(){
-        this.sample++;
+        setSample(sample + 1);
     }
 
     public void decreaseSample(){
-        this.sample--;
+        setSample(sample - 1);
     }
 }
