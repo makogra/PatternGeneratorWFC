a483a921932c197bc5a59cc0d0f0582fca71980e
makogra <pietrzak.maciek.pv@gmail.com>
2022-06-27 18:25:38 +0200

    successful launch

--- app/src/main/java/com/mako/patterngeneratorwfc/activities/AddTileSetActivity.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/activities/AddTileSetActivity.java
@@ -1,15 +1,35 @@
 package com.mako.patterngeneratorwfc.activities;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.mako.patterngeneratorwfc.R;
 
 public class AddTileSetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tile_set);
 
+        ImageButton cancelBtn = findViewById(R.id.cancel_button);
+        cancelBtn.setOnClickListener(v -> {
+            //TODO cancel end exit
+        });
+
+        ImageButton saveBtn = findViewById(R.id.save_button);
+        saveBtn.setOnClickListener(v -> {
+            //TODO save and return
+        });
 
     }
 
+    @Override
+    public void setSupportActionBar(@Nullable Toolbar toolbar) {
+        super.setSupportActionBar(toolbar);
+    }
 }
