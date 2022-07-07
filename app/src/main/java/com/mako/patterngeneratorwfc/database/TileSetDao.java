bad77d4efe9a027b7cbcf5ad21b395e5de36c002
makogra <pietrzak.maciek.pv@gmail.com>
2022-07-07 10:19:17 +0200

    added deleteAll(), getTileSetList(), count()

--- app/src/main/java/com/mako/patterngeneratorwfc/database/TileSetDao.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/database/TileSetDao.java
@@ -13,12 +13,22 @@ import java.util.List;
 @Dao
 public interface TileSetDao {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE, entity = TileSet.class)
     void insert(TileSet tileSet);
+
+    @Query("SELECT * FROM tileset_table ORDER BY id")
+    LiveData<List<TileSet>> getTileSetList();
 
     @Query("SELECT id FROM tileset_table")
     LiveData<List<String>> getAllIds();
 
     @Query("SELECT * FROM tileset_table WHERE id = :id")
-    TileSet getTileSet(String id);
+    LiveData<TileSet> getTileSet(String id);
+
+    @Query("SELECT COUNT(*) FROM tileset_table")
+    LiveData<Integer> count();
+
+    @Query("DELETE FROM tileset_table")
+    void deleteAll();
+
 }
