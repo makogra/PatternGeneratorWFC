26c58809a02853f9dc72f76e57b415c537a55635
makogra <pietrzak.maciek.pv@gmail.com>
2022-07-12 17:50:19 +0200

    added WFCFragment

---
+++ app/src/main/java/com/mako/patterngeneratorwfc/datamodels/WFCViewModel.java
@@ -0,0 +1,12 @@
+package com.mako.patterngeneratorwfc.datamodels;
+
+import androidx.lifecycle.ViewModel;
+
+public class WFCViewModel extends ViewModel {
+
+    //save output
+    private String string = "connection test";
+
+    public WFCViewModel(){
+    }
+}
---
+++ app/src/main/java/com/mako/patterngeneratorwfc/ui/WFCFragment.java
@@ -0,0 +1,45 @@
+package com.mako.patterngeneratorwfc.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mako.patterngeneratorwfc.R;
+import com.mako.patterngeneratorwfc.datamodels.SettingsTileSetViewModel;
+import com.mako.patterngeneratorwfc.datamodels.TileSetViewModel;
+import com.mako.patterngeneratorwfc.datamodels.WFCViewModel;
+
+public class WFCFragment extends Fragment {
+
+    private TileSetViewModel mTileSetViewModel;
+    private SettingsTileSetViewModel mSettingsTileSetViewModel;
+    private WFCViewModel mWFCViewModel;
+
+    public WFCFragment() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ViewModelProvider viewModelProvider = new ViewModelProvider(this);
+        mTileSetViewModel = viewModelProvider.get(TileSetViewModel.class);
+        mSettingsTileSetViewModel = viewModelProvider.get(mTileSetViewModel.getCurrentId(), SettingsTileSetViewModel.class);
+        mWFCViewModel = viewModelProvider.get(mTileSetViewModel.getCurrentId(), WFCViewModel.class);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_w_f_c, container, false);
+
+        return view;
+    }
+}
