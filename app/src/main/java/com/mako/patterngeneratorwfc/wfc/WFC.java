40e6b3c1c4102c7948c2c356b324b82e6222e1c5
makogra <pietrzak.maciek.pv@gmail.com>
2022-07-14 07:51:18 +0200

    basic init

--- app/src/main/java/com/mako/patterngeneratorwfc/wfc/WFC.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/wfc/WFC.java
@@ -2,4 +2,39 @@ package com.mako.patterngeneratorwfc.wfc;
 
 public class WFC {
 
+
+    private final int[][] valueGrid;
+    private final int patternSize;
+    private final int tilesOverLap;
+    private final int outputHeight;
+    private final int outputWidth;
+
+    public WFC(int[][] valueGrid, int patternSize, int tilesOverLap, int outputHeight, int outputWidth) {
+        this.valueGrid = valueGrid;
+        this.patternSize = patternSize;
+        this.tilesOverLap = tilesOverLap;
+        this.outputHeight = outputHeight;
+        this.outputWidth = outputWidth;
+    }
+
+    //Getters
+    public int[][] getValueGrid() {
+        return valueGrid;
+    }
+
+    public int getPatternSize() {
+        return patternSize;
+    }
+
+    public int getTilesOverLap() {
+        return tilesOverLap;
+    }
+
+    public int getOutputHeight() {
+        return outputHeight;
+    }
+
+    public int getOutputWidth() {
+        return outputWidth;
+    }
 }
