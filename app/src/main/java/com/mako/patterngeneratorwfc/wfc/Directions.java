37b01e9774d8a750e1610f8252c3df79d75da8e6
makogra <pietrzak.maciek.pv@gmail.com>
2022-07-14 10:01:12 +0200

    added Directions

---
+++ app/src/main/java/com/mako/patterngeneratorwfc/wfc/Directions.java
@@ -0,0 +1,74 @@
+package com.mako.patterngeneratorwfc.wfc;
+
+public class Directions {
+
+    private static final int[] UP = {-1, 0};
+    private static final int[] UP_RIGHT = {-1, 1};
+    private static final int[] RIGHT = {0, 1};
+    private static final int[] DOWN_RIGHT = {1, 1};
+    private static final int[] DOWN = {1, 0};
+    private static final int[] DOWN_LEFT = {1, -1};
+    private static final int[] LEFT = {0, -1};
+    private static final int[] UP_LEFT = {-1, -1};
+
+    private static final int[][] DIRECTIONS = {UP, UP_RIGHT, RIGHT, DOWN_RIGHT, DOWN, DOWN_LEFT, LEFT, UP_LEFT};
+
+    private static final int TOTAL_DIRECTIONS_NUMBER = DIRECTIONS.length;
+
+    public static int[] getUP() {
+        return UP;
+    }
+
+    public static int[] getUP_RIGHT() {
+        return UP_RIGHT;
+    }
+
+    public static int[] getRIGHT() {
+        return RIGHT;
+    }
+
+    public static int[] getDOWN_RIGHT() {
+        return DOWN_RIGHT;
+    }
+
+    public static int[] getDOWN() {
+        return DOWN;
+    }
+
+    public static int[] getDOWN_LEFT() {
+        return DOWN_LEFT;
+    }
+
+    public static int[] getLEFT() {
+        return LEFT;
+    }
+
+    public static int[] getUP_LEFT() {
+        return UP_LEFT;
+    }
+
+    public static int[][] getDIRECTIONS() {
+        return DIRECTIONS;
+    }
+
+    public static int getTOTAL_DIRECTIONS_NUMBER() {
+        return TOTAL_DIRECTIONS_NUMBER;
+    }
+
+    public static int[] oppositeDirection(int[] direction){
+        return new int[]{-direction[0], -direction[1]};
+    }
+
+    public  static int[] oppositeDirection(int directionIndex){
+        int index = (directionIndex + TOTAL_DIRECTIONS_NUMBER /2) % DIRECTIONS.length;
+        return DIRECTIONS[index];
+    }
+
+    public static int oppositeDirectionIndex(int directionIndex){
+        return (directionIndex + TOTAL_DIRECTIONS_NUMBER / 2) % DIRECTIONS.length;
+    }
+
+    public static int[] getDirection(int directionIndex) {
+        return DIRECTIONS[directionIndex];
+    }
+}
---
+++ app/src/test/java/com/mako/patterngeneratorwfc/wfc/DirectionsTest.java
@@ -0,0 +1,43 @@
+package com.mako.patterngeneratorwfc.wfc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DirectionsTest {
+
+    @Test
+    void testOppositeDirection() {
+        assertAll(
+                () -> assertArrayEquals(Directions.getUP(), Directions.oppositeDirection(Directions.getDOWN())),
+                () -> assertArrayEquals(Directions.getUP_LEFT(), Directions.oppositeDirection(Directions.getDOWN_RIGHT())),
+                () -> assertArrayEquals(Directions.getRIGHT(), Directions.oppositeDirection(Directions.getLEFT()))
+        );
+
+        assertAll(
+                () -> assertArrayEquals(Directions.getUP(), Directions.oppositeDirection(4)),
+                () -> assertArrayEquals(Directions.getUP_RIGHT(), Directions.oppositeDirection(5)),
+                () -> assertArrayEquals(Directions.getRIGHT(), Directions.oppositeDirection(6)),
+                () -> assertArrayEquals(Directions.getDOWN_RIGHT(), Directions.oppositeDirection(7)),
+                () -> assertArrayEquals(Directions.getDOWN(), Directions.oppositeDirection(0)),
+                () -> assertArrayEquals(Directions.getDOWN_LEFT(), Directions.oppositeDirection(1)),
+                () -> assertArrayEquals(Directions.getLEFT(), Directions.oppositeDirection(2)),
+                () -> assertArrayEquals(Directions.getUP_LEFT(), Directions.oppositeDirection(3))
+        );
+    }
+
+    @Test
+    void testOppositeDirectionIndex(){
+        assertAll(
+                () -> assertEquals(0, Directions.oppositeDirectionIndex(4)),
+                () -> assertEquals(1, Directions.oppositeDirectionIndex(5)),
+                () -> assertEquals(2, Directions.oppositeDirectionIndex(6)),
+                () -> assertEquals(3, Directions.oppositeDirectionIndex(7)),
+                () -> assertEquals(4, Directions.oppositeDirectionIndex(0)),
+                () -> assertEquals(5, Directions.oppositeDirectionIndex(1)),
+                () -> assertEquals(6, Directions.oppositeDirectionIndex(2)),
+                () -> assertEquals(7, Directions.oppositeDirectionIndex(3))
+        );
+    }
+
+}
