16f7fe91a58c9e8572c4c36e5aac5102df8cb46e
makogra <pietrzak.maciek.pv@gmail.com>
2022-07-14 17:39:17 +0200

    added EntryPattern static nested class

--- app/src/main/java/com/mako/patterngeneratorwfc/wfc/Propagator.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/wfc/Propagator.java
@@ -1,6 +1,25 @@
 package com.mako.patterngeneratorwfc.wfc;
 
 public class Propagator {
+
+    private static class EntryPattern{
+        int row;
+        int col;
+        int pattern;
+        int directionIndex;
+        boolean propagateEnabler;
+
+        public EntryPattern(int row, int col, int pattern, int directionIndex, boolean propagateEnabler) {
+            this.row = row;
+            this.col = col;
+            this.pattern = pattern;
+            this.directionIndex = directionIndex;
+            this.propagateEnabler = propagateEnabler;
+        }
+
+
+    }
+
     public void addToPropagate(int row, int col, int patternId, boolean b) {
         return;
     }
