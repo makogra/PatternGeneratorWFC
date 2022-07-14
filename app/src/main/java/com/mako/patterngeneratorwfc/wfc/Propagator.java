69c2e0679798954ea98692e74a250ceaa3352946
makogra <pietrzak.maciek.pv@gmail.com>
2022-07-14 17:53:30 +0200

    added propagate methods

--- app/src/main/java/com/mako/patterngeneratorwfc/wfc/Propagator.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/wfc/Propagator.java
@@ -31,7 +31,51 @@ public class Propagator {
         propagateQueue = new LinkedList<>();
     }
 
-    public void addToPropagate(int row, int col, int patternId, boolean b) {
-        return;
+    void propagate(){
+        EntryPattern entryPattern;
+        while (!propagateQueue.isEmpty()) {
+            entryPattern = propagateQueue.poll();
+            propagateAt(entryPattern);
+        }
+    }
+
+    void propagateAt(EntryPattern entry){
+        int row = entry.row;
+        int col = entry.col;
+        int pattern = entry.pattern;
+        int directionIndex = entry.directionIndex;
+        boolean propagateEnabler = entry.propagateEnabler;
+        Cell cell = wave.getCell(row, col);
+
+        if (cell.isObserved())
+            return;
+
+        if (propagateEnabler){
+            cell.removePatternFromPatternEnablers(directionIndex, pattern);
+        } else {
+            cell.removePatternEnablesExceptPattern(directionIndex, pattern);
+        }
+
+        wave.addToLowestEntropyQueue(row, col);
+
+    }
+
+    void addToPropagate(int row, int col, int pattern, boolean propagateEnabler){
+        int patternRow;
+        int patternCol;
+        int[] direction;
+        int[][] directions = Directions.getDIRECTIONS();
+        int maxHeight = wave.getHeight();
+        int maxWidth = wave.getWidth();
+        for (int directionIndex = 0; directionIndex < directions.length; directionIndex++) {
+            direction = directions[directionIndex];
+            patternRow = row + direction[0];
+            patternCol = col + direction[1];
+
+            if (patternRow < 0 || patternRow >= maxHeight || patternCol < 0 || patternCol >= maxWidth || wave.getCell(patternRow, patternCol).isObserved())
+                continue;
+
+            propagateQueue.add(new EntryPattern(patternRow, patternCol, pattern, Directions.oppositeDirectionIndex(directionIndex), propagateEnabler));
+        }
     }
 }
