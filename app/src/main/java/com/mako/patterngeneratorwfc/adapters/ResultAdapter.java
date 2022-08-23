2e4f1bf8feb37b6fb22a2386d5141100ac41deee
makogra <pietrzak.maciek.pv@gmail.com>
2022-08-23 07:19:38 +0200

    added get pixelAt() and get PatternAt()

--- app/src/main/java/com/mako/patterngeneratorwfc/adapters/ResultAdapter.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/adapters/ResultAdapter.java
@@ -1,11 +1,7 @@
 package com.mako.patterngeneratorwfc.adapters;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,8 +12,9 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mako.patterngeneratorwfc.R;
+import com.mako.patterngeneratorwfc.ui.ResultFragment;
 
-import java.util.Random;
+import java.util.List;
 
 public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.Holder> {
 
@@ -25,13 +22,17 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.Holder> {
     private final int height;
     private final int width;
     private final int patternSize;
+    private final int overlap;
+    private final List<Integer[][]> patternList;
     private int[][] grid;
 
-    public ResultAdapter(int[][] grid, int width, int height, int patternSize) {
-        this.grid = grid;
-        this.height = height;
-        this.width = width;
-        this.patternSize = patternSize;
+    public ResultAdapter(ResultFragment resultFragment) {
+        this.grid = resultFragment.getOutputGrid();
+        this.height = resultFragment.getHeight();
+        this.width = resultFragment.getWidth();
+        this.patternSize = resultFragment.getPatternSize();
+        this.overlap = resultFragment.getOverlap();
+        this.patternList = resultFragment.getPatternList();
     }
 
     @NonNull
@@ -47,6 +48,8 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.Holder> {
         // Create random color to test if this function is working correctly
         int r,g,b;
         Bitmap bitmap = Bitmap.createBitmap(patternSize, patternSize,Bitmap.Config.ARGB_8888);
+
+
         r = 255  - (position % 255) /2;
         g = 0;
         b = position % 255;
@@ -55,7 +58,32 @@ public class ResultAdapter extends RecyclerView.Adapter<ResultAdapter.Holder> {
                 bitmap.setPixel(i,j, Color.rgb(r,g,b));
             }
         }
+
+        Log.d(TAG, "onBindViewHolder: position = " + position);
         holder.imageView.setImageBitmap(bitmap);
+    }
+
+    private int pixelAt(int position){
+        // Get pixels at
+        int[] patternIndex = getPatternIndexAt(position);
+        int patternId = grid[patternIndex[0]][patternIndex[1]];
+        Integer[][] pattern = patternList.get(patternId);
+        return 0;
+    }
+
+    private int[] getPatternIndexAt(int position) {
+        // pattern =
+        // position = 17
+        // width = 16
+        // patternSize = 3
+        // overlap = 1
+        // patternRowIndex should equals 0
+        // (33 % 16) / (3 - 1)
+        // 1 / 2 = 0
+
+        int patternRowIndex = (position + 1) / width / (patternSize - overlap);
+        int patternColIndex = (position % width) / (patternSize - overlap);
+        return new int[]{patternRowIndex, patternColIndex};
     }
 
     @Override
