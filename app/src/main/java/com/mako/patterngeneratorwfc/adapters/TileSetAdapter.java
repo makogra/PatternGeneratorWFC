2ca8962e638141be0ed6e032992571dd03c27f5f
makogra <pietrzak.maciek.pv@gmail.com>
2022-09-13 11:26:06 +0200

    feat added thumbnails

--- app/src/main/java/com/mako/patterngeneratorwfc/adapters/TileSetAdapter.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/adapters/TileSetAdapter.java
@@ -1,5 +1,9 @@
 package com.mako.patterngeneratorwfc.adapters;
 
+import android.graphics.Bitmap;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +21,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.mako.patterngeneratorwfc.R;
 import com.mako.patterngeneratorwfc.TileSet;
 import com.mako.patterngeneratorwfc.datamodels.TileSetViewModel;
+import com.mako.patterngeneratorwfc.wfc.Colors;
 
 public class TileSetAdapter extends ListAdapter<TileSet, TileSetAdapter.ViewHolder> {
 
@@ -25,6 +30,8 @@ public class TileSetAdapter extends ListAdapter<TileSet, TileSetAdapter.ViewHold
 
     private final TileSetViewModel tileSetViewModel;
     private FrameLayout currentFocusedFrameLayout;
+    private int height;
+    private int width;
 
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener {
 
@@ -59,12 +66,16 @@ public class TileSetAdapter extends ListAdapter<TileSet, TileSetAdapter.ViewHold
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.cardview_tile_set, parent, false);
+        ImageView imageView = v.findViewById(R.id.cardView_tile_set_preview_imageView);
+        height = imageView.getLayoutParams().height;
+        width = imageView.getLayoutParams().width;
         return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         TileSet current = getItem(position);
+        createThumbnail(holder, current);
         holder.textView.setText(current.getTileId());
         if (current.getTileId().equals(tileSetViewModel.getCurrentId())){
             holder.frameLayout.setBackgroundResource(R.color.active);
@@ -77,6 +88,36 @@ public class TileSetAdapter extends ListAdapter<TileSet, TileSetAdapter.ViewHold
             currentFocusedFrameLayout = holder.frameLayout;
             tileSetViewModel.setCurrentId(current.getTileId());
         });
+    }
+
+    private void createThumbnail(@NonNull ViewHolder holder, TileSet current) {
+        Log.d(TAG, "createThumbnail: " + current);
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                Bitmap thumbnail = getThumbnail(current);
+                new Handler(Looper.getMainLooper()).post(() -> holder.imageView.setImageBitmap(thumbnail));
+                //holder.imageView.post(() -> holder.imageView.setImageBitmap(thumbnail));
+            }
+
+            private Bitmap getThumbnail(TileSet tileSet){
+                Bitmap bitmap = Bitmap.createBitmap(tileSet.getTileSetWidth(), tileSet.getTileSetHeight(), Bitmap.Config.ARGB_8888);
+                int[][] valueGrid = tileSet.getValueGrid();
+
+                for (int i = 0; i < valueGrid.length; i++) {
+                    for (int j = 0; j < valueGrid[0].length; j++) {
+                        bitmap.setPixel(j, i, getColor(valueGrid[i][j], tileSet));
+                    }
+                }
+                //return bitmap;
+                return Bitmap.createScaledBitmap(bitmap, width, height, false);
+            }
+
+            private int getColor(int value, TileSet tileSet){
+                return Colors.getValue(tileSet.getValueToStringPath().get(value));
+            }
+        }).start();
     }
 
     private void defocusPastTileSet() {
