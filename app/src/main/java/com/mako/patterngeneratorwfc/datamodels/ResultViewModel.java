d1d57cc1b2c7aab107dc4b6ec2a11f108106e848
makogra <pietrzak.maciek.pv@gmail.com>
2022-11-12 10:08:01 +0100

    style doc and to do

--- app/src/main/java/com/mako/patterngeneratorwfc/datamodels/ResultViewModel.java
+++ app/src/main/java/com/mako/patterngeneratorwfc/datamodels/ResultViewModel.java
@@ -7,7 +7,10 @@ import androidx.lifecycle.ViewModel;
 public class ResultViewModel extends ViewModel {
 
 
+    // small bitmap that represent all needed information about the output
     private Bitmap bitmap;
+    //TODO find usage or delete
+    // temporary redundant and unused
     private int height;
     private int width;
 
