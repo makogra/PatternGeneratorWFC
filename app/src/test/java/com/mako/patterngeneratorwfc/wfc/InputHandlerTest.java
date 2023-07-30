ab61b265454905a7c4bc4fd74bf2a9e630293d50
makogra <pietrzak.maciek.pv@gmail.com>
2023-07-30 17:48:34 +0200

    fix getPatternList test method

--- app/src/test/java/com/mako/patterngeneratorwfc/wfc/InputHandlerTest.java
+++ app/src/test/java/com/mako/patterngeneratorwfc/wfc/InputHandlerTest.java
@@ -1,14 +1,12 @@
 package com.mako.patterngeneratorwfc.wfc;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import androidx.annotation.Nullable;
-
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class InputHandlerTest {
@@ -21,18 +19,22 @@ public class InputHandlerTest {
     @Before
     public void SetUp(){
         inputGrid = new String[][]{
+                {"G", "G", "G", "G", "C", "S"},
                 {"G", "G", "G", "C", "C", "S"},
-                {"G", "G", "C", "C", "S", "S"},
-                {"G", "G", "C", "S", "S", "S"}
+                {"G", "G", "G", "C", "S", "S"},
+                {"G", "G", "G", "C", "S", "S"},
+                {"G", "G", "G", "C", "S", "S"}
         };
         inputGridValue = new int[][]{
+                {0,0,0,0,1,2},
                 {0,0,0,1,1,2},
-                {0,0,1,1,2,2},
-                {0,0,1,2,2,2},
+                {0,0,0,1,2,2},
+                {0,0,0,1,2,2},
+                {0,0,0,1,2,2},
         };
-        patternSize = 2;
+        patternSize = 4;
         boolean rotation = true;
-        boolean reflection = true;
+        boolean reflection = false;
 
         inputHandler = new InputHandler(inputGridValue, patternSize, rotation, reflection);
     }
@@ -40,9 +42,11 @@ public class InputHandlerTest {
     @Test
     public void getValueGrid() {
         int[][] shouldBe = new int[][]{
+                {0,0,0,0,1,2},
                 {0,0,0,1,1,2},
-                {0,0,1,1,2,2},
-                {0,0,1,2,2,2},
+                {0,0,0,1,2,2},
+                {0,0,0,1,2,2},
+                {0,0,0,1,2,2},
         };
 
         assertArrayEquals(shouldBe, inputHandler.getValueGrid());
@@ -63,72 +67,44 @@ public class InputHandlerTest {
 
     @Test
     public void getPatternList() {
-        List<Integer[][]> shouldBe = new ArrayList<Integer[][]>(){
-            @Override
-            public boolean contains(Object o) {
-                if (!(o instanceof Integer[][]))
-                    return false;
 
-                Integer[][] ints = (Integer[][]) o;
-
-                LABEL: for (Integer[][] integers : this){
-                    for (int i = 0; i < integers.length; i++) {
-                        for (int j = 0; j < integers[0].length; j++) {
-                            if (integers[i][j].intValue() != ints[i][j].intValue())
-                                continue LABEL;
-                        }
-                    }
-                    return true;
-                }
-                return false;
-            }
-        };
-        shouldBe.add(new Integer[][]{{0,0}, {0,0}});
-        shouldBe.add(new Integer[][]{{0,0}, {0,1}});
-        shouldBe.add(new Integer[][]{{0,1}, {1,1}});
-        shouldBe.add(new Integer[][]{{1,1}, {1,2}});
-        shouldBe.add(new Integer[][]{{1,2}, {2,2}});
-        shouldBe.add(new Integer[][]{{0,1}, {0,1}});
-        shouldBe.add(new Integer[][]{{2,2}, {2,2}});
-
-        //Rotations and Reflections
-
-        shouldBe.add(new Integer[][]{{0,0}, {1,0}});
-        shouldBe.add(new Integer[][]{{0,1}, {0,0}});
-        shouldBe.add(new Integer[][]{{1,0}, {0,0}});
-
-        shouldBe.add(new Integer[][]{{1,0}, {1,1}});
-        shouldBe.add(new Integer[][]{{1,1}, {0,1}});
-        shouldBe.add(new Integer[][]{{1,1}, {1,0}});
-
-        shouldBe.add(new Integer[][]{{1,1}, {2,1}});
-        shouldBe.add(new Integer[][]{{1,2}, {1,1}});
-        shouldBe.add(new Integer[][]{{2,1}, {1,1}});
-
-        shouldBe.add(new Integer[][]{{2,1}, {2,2}});
-        shouldBe.add(new Integer[][]{{2,2}, {1,2}});
-        shouldBe.add(new Integer[][]{{2,2}, {2,1}});
-
-        shouldBe.add(new Integer[][]{{1,0}, {1,0}});
-        shouldBe.add(new Integer[][]{{1,1}, {0,0}});
-        shouldBe.add(new Integer[][]{{0,0}, {1,1}});
-
+        Integer[][][] shouldBeArray = new Integer[][][]{{{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 0}},
+                {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 1}},
+                {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 1, 1}},
+                {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 1}, {2, 2, 2, 1}},
+                {{0, 0, 0, 0}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}},
+                {{0, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 1, 1}, {2, 2, 2, 2}},
+                {{0, 0, 0, 0}, {1, 1, 1, 1}, {2, 2, 2, 1}, {2, 2, 2, 2}},
+                {{0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}},
+                {{0, 0, 0, 1}, {0, 0, 1, 1}, {0, 0, 1, 2}, {0, 0, 1, 2}},
+                {{0, 0, 1, 1}, {0, 0, 1, 2}, {0, 0, 1, 2}, {0, 0, 1, 2}},
+                {{0, 0, 1, 2}, {0, 1, 1, 2}, {0, 1, 2, 2}, {0, 1, 2, 2}},
+                {{0, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+                {{0, 1, 1, 2}, {0, 1, 2, 2}, {0, 1, 2, 2}, {0, 1, 2, 2}},
+                {{1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {0, 0, 0, 0}},
+                {{1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}},
+                {{1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+                {{1, 1, 2, 2}, {0, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+                {{1, 2, 2, 2}, {1, 1, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+                {{2, 1, 0, 0}, {2, 1, 0, 0}, {1, 1, 0, 0}, {1, 0, 0, 0}},
+                {{2, 1, 0, 0}, {2, 1, 0, 0}, {2, 1, 0, 0}, {1, 1, 0, 0}},
+                {{2, 2, 1, 0}, {2, 2, 1, 0}, {2, 1, 1, 0}, {2, 1, 0, 0}},
+                {{2, 2, 1, 0}, {2, 2, 1, 0}, {2, 2, 1, 0}, {2, 1, 1, 0}},
+                {{2, 2, 2, 2}, {1, 1, 2, 2}, {0, 1, 1, 1}, {0, 0, 0, 0}},
+                {{2, 2, 2, 2}, {1, 2, 2, 2}, {1, 1, 1, 1}, {0, 0, 0, 0}}};
 
         List<Integer[][]> actual = inputHandler.getPatternList();
 
-        assertEquals(shouldBe.size(), actual.size());
+        assertEquals(shouldBeArray.length, actual.size());
 
-        Integer[][] actualPattern;
-        for (int i = 0; i < actual.size(); i++) {
-            actualPattern = actual.get(i);
-            assertTrue(shouldBe.contains(actualPattern));
+        for (Integer[][] shouldBePattern : shouldBeArray){
+            assertTrue(actual.contains(shouldBePattern));
         }
     }
 
     @Test
     public void getTotalNumberOfPatterns() {
-
-        assertEquals(22, inputHandler.getTotalNumberOfPatterns());
+        assertEquals(24, inputHandler.getTotalNumberOfPatterns());
     }
 
 }
